af74cae50b83d9870771552a2ca04cdf4af59d00
Jordan Lewallen <support@uncvrd.co>
2023-11-13 19:51:10 -0800

    adds pythonunbuffered to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
 
 COPY ./app ./app
 
+ENV PYTHONUNBUFFERED=1
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--reload", "--log-level", "debug"]
--- app/main.py
+++ app/main.py
@@ -9,18 +9,23 @@ import laion_clap
 from typing import List
 from transformers import RobertaModel
 
-model: laion_clap.CLAP_Module | None = None
+# model: laion_clap.CLAP_Module | None = None
+
+ml_models = {}
 
 # Loads the model when the app starts
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
+    # took ml_models concept from https://fastapi.tiangolo.com/advanced/events/#lifespan
     # anything before yield is loaded on startup
-    model = laion_clap.CLAP_Module(enable_fusion=False, amodel="HTSAT-base")
+    ml_models["clap"] = laion_clap.CLAP_Module(enable_fusion=False, amodel="HTSAT-base")
     # Make sure you've downloaded this checkpoint from here and store it in the /clap-data directory in this project
     # https://huggingface.co/lukewys/laion_clap/resolve/main/music_speech_audioset_epoch_15_esc_89.98.pt?download=true 
-    model.load_ckpt("/clap-data/music_speech_audioset_epoch_15_esc_89.98.pt")  
+    ml_models["clap"].load_ckpt("/clap-data/music_speech_audioset_epoch_15_esc_89.98.pt")
     yield
     # Anything after yield is run on teardown
+    ml_models.clear()
 
 app = FastAPI(lifespan=lifespan)
 
@@ -34,10 +39,6 @@ class TextEmbeddingModel(BaseModel):
 @app.post("/text-embedding")
 def post_text_embedding(body: TextEmbeddingModel): 
 
-    embeddings = []
-
-    if model is not None:
-        embeddings = model.get_text_embedding(body.queries)
-        embeddings = embeddings.tolist()
+    embeddings = ml_models["clap"].get_text_embedding(body.queries).tolist()
 
     return {"embedding": embeddings}
